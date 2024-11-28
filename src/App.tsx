3ba70e8d91d695ccd294bcaeece158804aaa504d
Owen Burnett <oburnett127@gmail.com>
2024-11-28 02:23:47 -0600

    Moved privacy policy and tos to public folder text files. Mod cookiepolicypage component. Chgd global styling for mobile friendliness.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
-import ErrorPage from './pages/ErrorPage';
-import Footer from './components/Footer';
-import PrivacyPolicyPage from './pages/PrivacyPolicy';
-import TermsOfServicePage from './pages/TermsOfServicePage';
-import CookiePolicyPage from './pages/CookiePolicyPage';
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+const ErrorPage = lazy(() => import('./pages/ErrorPage'));
+const Footer = lazy(() => import('./components/Footer'));
+const PrivacyPolicyPage = lazy(() => import('./pages/PrivacyPolicy'));
+const TermsOfServicePage = lazy(() => import('./pages/TermsOfServicePage'));
+const CookiePolicyPage = lazy(() => import('./pages/CookiePolicyPage'));
 
 function App() {
 
   return (   
     <div className={"App"}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<HomePage />}></Route>
-          <Route path="privacy-policy" element={<PrivacyPolicyPage />}></Route>
-          <Route path="terms-of-service" element={<TermsOfServicePage />}></Route>
-          <Route path="cookie-policy" element={<CookiePolicyPage />}></Route>
-          <Route path="/error" element={<ErrorPage />}></Route>
-          <Route path="*" element={<NotFoundPage />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    <Footer />
+      <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/">
+            <Route index element={<HomePage />}></Route>
+            <Route path="privacy-policy" element={<PrivacyPolicyPage />}></Route>
+            <Route path="terms-of-service" element={<TermsOfServicePage />}></Route>
+            <Route path="cookie-policy" element={<CookiePolicyPage />}></Route>
+            <Route path="/error" element={<ErrorPage />}></Route>
+            <Route path="*" element={<NotFoundPage />}></Route>
+          </Route>
+        </Routes>
+        </Suspense>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
--- src/pages/TermsOfServicePage.tsx
+++ src/pages/TermsOfServicePage.tsx
@@ -1,73 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-function TermsOfServicePage() {
+const TermsOfServicePage = () => {
+  const [termsOfServiceText, setTermsOfServiceText] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/terms-of-service.txt")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch the terms of service.");
+        }
+        return response.text();
+      })
+      .then((text) => {
+        setTermsOfServiceText(text);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error loading privacy policy:", error);
+        setTermsOfServiceText("Sorry, we couldn't load the terms of service at this time.");
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading terms of service...</p>;
+  }
+
   return (
-    <>
-        <h1>Terms of Service</h1>
-<h2>Updated on 06-19-2024</h2>
-<h2>1. Introduction</h2>
-Welcome to the website of Ultrawave Interactive LLC. These Terms of Service ("ToS") govern your use of our website located at https://ultrawaveinteractive.com (the "Site"). By accessing or using our Site, you agree to comply with and be bound by these terms.
-
-<h2>2. Acceptance of Terms</h2>
-By using our Site, you agree to these ToS. If you do not agree, please do not use our Site.
-
-<h2>3. Modifications to Terms</h2>
-We reserve the right to modify these ToS at any time. Your continued use of the Site following any changes indicates your acceptance of the new terms.
-
-<h2>4. Services Provided</h2>
-We offer web design services including designing, developing, and producing a website consistent with Client’s requested specifications (the “Work”). Upon completion of the Work, Contractor shall deliver possession and control of the website to Client. Contractor is not obligated to perform any maintenance or alterations to the website after Client receives possession of the website. Certain Work may be performed pursuant to industry standards, other aspects of the Work may be customized and unique to Client, as determined by Contractor. We do not guarantee specific results or outcomes.
-
-<h2>5. Use of Website</h2>
-You agree to use the Site only for lawful purposes and in a way that does not infringe on the rights of others or restrict their use and enjoyment of the Site.
-
-<h2>6. Intellectual Property</h2>
-All content on the Site, including text, graphics, logos, and images is protected by copyright laws. Unauthorized use is prohibited.
-
-<h2>7. User Content</h2>
-Users may submit content such as comments or reviews. By submitting content, you grant us the right to use, modify, and display your content on the Site.
-
-<h2>8. Links to Other Websites</h2>
-Our Site may contain links to third-party websites. We are not responsible for the content or practices of these sites.
-
-<h2>9. Disclaimers</h2>
-The Site and services are provided "as is" without warranties of any kind. We disclaim all warranties, express or implied.
-
-<h2>10. Limitation of Liability</h2>
-We will not be liable for any damages arising from the use of the Site or services.
-
-<h2>11. Indemnification</h2>
-You agree to indemnify and hold Ultrawave Interactive LLC harmless from any claims, damages, or expenses arising from your use of the Site.
-
-<h2>12. Governing Law</h2>
-These ToS are governed by the laws of The United States of America and The State of Oklahoma. Any disputes will be resolved in the courts of The State of Oklahoma.
-
-<h2>13. Contact Information</h2>
-For questions or concerns about these ToS, please contact us at oburnett127@gmail.com.
-
-<h2>14. Termination</h2>
-We reserve the right to terminate or suspend your access to the Site at any time, without notice, for any reason.
-
-<h2>15. Severability</h2>
-If any provision of these ToS is found to be invalid, the remaining provisions will remain in effect.
-
-<h2>16. Additional Terms</h2>
-Client acknowledges that if it delivers text or images to be included on Client’s website, Contractor is not liable for any third-party claims or demands related to the use of the text or images. Client shall be solely responsible for confirming the copyright of any such images or text.
-
-Client acknowledges and agrees that any website or data which is accessible through the internet is subject to third parties attempting to access, or accessing, private or sensitive information and Client shall be solely responsible for purchasing or otherwise providing security for its website.
-
-Client acknowledges and agrees that Contractor is not responsible for providing any maintenance services and is not responsible for any future costs of the website which may be incurred through hosting fees, or the use of any applications or software associated with the website.
-
-<h2>16. Arbitration</h2>
-All disputes of whatever kind between Contractor and Client, based on past, present or future acts, whether known or unknown, and arising out of or relating to the formation or performance of this Agreement shall be resolved by final and binding arbitration. The arbitration shall be conducted in Oklahoma City, Oklahoma, pursuant to the Expedited Procedures of the Commercial Arbitration Rules of the American Arbitration Association. and shall be governed by the Federal Arbitration Act. Client acknowledges that, with respect to all such disputes, it has voluntarily and knowingly waived any right it may have to a jury trial. Each party agrees that in any arbitration or litigation it shall be limited to the recovery of actual damages and waives the right to claim consequential damages, including “business down-time,” and punitive or exemplary damages
-
-<h2>17. Disclaimer: Security and Privacy</h2>
-Contractor shall utilize industry standards and take all reasonable measures to ensure the safety, security, and privacy of Client’s personal information, hardware, and software. However, Client is advised that due to the nature of the internet; any hardware, software, or personal information on systems accessible to the internet are subject to the attempts of third parties’ to improperly access the Client’s information and systems, including third-party systems which provide storage, security, and “backup” to Client. Contractor disclaims any liability for any third party’s breach of Client’s personal information, software or hardware. Other than in the instance of gross negligence, Client hereby waives and releases Contractor from liability for the actions or omissions of any third-party.
-
-Client acknowledges and agrees to the terms of the above Disclaimer and knowingly and voluntarily waives any claims arising from, or related to, a breach of Client’s personal information, hardware, or software by a third-party:
-
-Client acknowledges that it has been made aware that it may obtain Cyber Insurance at its own cost to protect its data, hardware, software, business operations, and personal or proprietary information which may be stored on Client’s computers, servers, backup servers, or other technology systems.
-    </>
+    <div>
+      <h1>Privacy Policy</h1>
+      <pre style={{ whiteSpace: "pre-wrap" }}>{termsOfServiceText}</pre>
+    </div>
   );
-}
+};
 
 export default TermsOfServicePage;
+
