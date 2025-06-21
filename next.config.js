f4dd25acefdd78192c684e7be1971a43e5349aa2
Owen Burnett <oburnett127@gmail.com>
2025-06-21 17:40:42 -0500

    Moded scripts in package.json so they dont use cross env

--- next.config.js
+++ next.config.js
@@ -8,7 +8,5 @@ module.exports = {
   env: {
     NEXT_PUBLIC_BACKEND_URL:
       process.env.NEXT_PUBLIC_BACKEND_URL || 'https://ultrawaveinteractive.com',
-    NEXTAUTH_SECRET:
-      process.env.NEXTAUTH_SECRET,
   },
 };
