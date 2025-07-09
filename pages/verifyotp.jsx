0c76d687773e17b9537a79f54ac183fa9e9e6ca6
Owen Burnett <oburnett127@gmail.com>
2025-07-09 14:37:58 -0500

    added import of getSession from next-auth/react

--- pages/verifyotp.jsx
+++ pages/verifyotp.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { getSession } from "next-auth/react";
 
 export default function VerifyOTP() {
   const [otp, setOtp]               = useState("");
