f45fd37462956d61a5289033e9aa7f4e83f71b05
Owen Burnett <oburnett127@gmail.com>
2025-10-22 18:15:51 -0500

    Changing text alignment on home page and on blog entry page

--- pages/index.jsx
+++ pages/index.jsx
@@ -59,7 +59,7 @@ function Home() {
 
       {/* Your normal content continues below */}
       <div className={styles.content}>
-        <p>I replace slow website builders with high-performance custom websites!</p>
+        <p className="centered-text">I replace slow website builders with high-performance custom websites!</p>
 
         <script
           type="application/ld+json"
@@ -108,11 +108,11 @@ function Home() {
             </button>
           </a>
 
-          <p>
+          <p className="centered-text">
             You don&apos;t pay me until you are satisfied with the quality of my
             work!
           </p>
-          <p>
+          <p className="centered-text">
             Fully customized websites, tailored to the specific and unique needs
             of your business!
           </p>
@@ -165,9 +165,9 @@ function Home() {
             </Stack>
           </Box>
 
-          <p>I believe in maintaining excellent relationships with my clients. When doing business with me you are entering into a partnership. I am dedicated to the success of your business.</p>
+          <p className="centered-text">I believe in maintaining excellent relationships with my clients. When doing business with me you are entering into a partnership. I am dedicated to the success of your business.</p>
 
-          <p>I work with a wide variety of businesses:</p>
+          <p className="centered-text">I work with a wide variety of businesses:</p>
 
           <Box display="flex" justifyContent="center" alignItems="center" minHeight="30vh">
             <List>
@@ -188,13 +188,13 @@ function Home() {
             </List>
           </Box>
 
-          <p>Let me help to increase the revenue of your business!</p>
+          <p className="centered-text">Let me help to increase the revenue of your business!</p>
 
           {/* <a className={styles.centerButton} href="https://ultrawavesample.com">
             <button className={styles.button}>See a sample of my work</button>
           </a> */}
 
-          <p>Contact me using the below form or call 405-519-8318 to schedule a free consultation!</p>
+          <p className="centered-text">Contact me using the below form or call 405-519-8318 to schedule a free consultation!</p>
 
           <hr />
           <div id="contact-form">
@@ -205,32 +205,32 @@ function Home() {
         <div className={styles.businessGrid}>
           <div className={styles.card}>
             <img src="/images/petcare.jpg" alt="Dog having fur groomed." />
-            <p>We would love to work on a website for your pet care business!</p>
+            <p className="centered-text">We would love to work on a website for your pet care business!</p>
           </div>
 
           <div className={styles.card}>
             <img src="/images/dentalhygienist.jpg" alt="Dental hygienist at work." />
-            <p>We work with dentists and some medical practices!</p>
+            <p className="centered-text">We work with dentists and some medical practices!</p>
           </div>
 
           <div className={styles.card}>
             <img src="/images/peopleingym.jpg" alt="People in a gym." />
-            <p>Your gym or fitness training business could really benefit from our expertise!</p>
+            <p className="centered-text">Your gym or fitness training business could really benefit from our expertise!</p>
           </div>
 
           <div className={styles.card}>
             <img src="/images/plumberlookingatpipe.jpg" alt="Plumber looking at a pipe." />
-            <p>Increasing the revenue of your plumbing business sounds like a good idea to us! Get ready!</p>
+            <p className="centered-text">Increasing the revenue of your plumbing business sounds like a good idea to us! Get ready!</p>
           </div>
 
           <div className={styles.card}>
             <img src="/images/hvacmenu.jpg" alt="Technician touches HVAC menu." />
-            <p>Your HVAC business needs the care it deserves!</p>
+            <p className="centered-text">Your HVAC business needs the care it deserves!</p>
           </div>
 
           <div className={styles.card}>
             <img src="/images/electricalreadingdevice.jpg" alt="Technician takes electrical reading using a device." />
-            <p>We have something special in store for your electrical business!</p>
+            <p className="centered-text">We have something special in store for your electrical business!</p>
           </div>
         </div>
 
