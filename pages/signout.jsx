89041a55b41be28c6a8f13b5a80b91dbdfa3b259
Owen Burnett <oburnett127@gmail.com>
2025-11-26 12:49:34 -0600

    Fixed signout.jsx so that it is an actual component

--- pages/signout.jsx
+++ pages/signout.jsx
@@ -1,5 +1,21 @@
+// pages/signout.jsx
+"use client";
+
 import { signOut } from "next-auth/react";
 
-<button onClick={() => signOut({ callbackUrl: "/" })}>
-  Sign Out
-</button>
+export default function SignOutPage() {
+  const handleSignOut = () => {
+    signOut({ callbackUrl: "/" });
+  };
+
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Are you sure you want to sign out?</h1>
+      <button
+        onClick={handleSignOut}
+      >
+        Sign Out
+      </button>
+    </div>
+  );
+}
